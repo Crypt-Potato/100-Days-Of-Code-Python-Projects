035e5f3e98c15083be6514150cf9d7f5dd05338a
arham shah <yugsha28@gmail.com>
2023-02-09 18:32:01 -0500

    Starting my Habit Tracker Project

--- Habit-Tracker/main.py
+++ Habit-Tracker/main.py
@@ -1,13 +1,9 @@
 import requests
-import os
 
-pixela_endpoint = "https://www.pixel.la/v1/users"
-
-user_token = os.environ["USER_TOKEN"]
-username = os.environ["USERNAME"]
+pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    'token': "ufsehfisg3392skefefj39fefje",
+    "token": "234959khskfhir3945h",
     "username": "yugm",
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
