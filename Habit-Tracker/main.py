539a72509dfd29065b2ec742b1850f8edaa5af48
arham shah <yugsha28@gmail.com>
2023-02-09 19:44:50 -0500

    2/9 day completed, pushing my code

--- Habit-Tracker/main.py
+++ Habit-Tracker/main.py
@@ -1,13 +1,21 @@
 import requests
+import os
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
+USERNAME = os.environ["USERNAME"]
+TOKEN = os.environ["USER_TOKEN"]
+
 user_params = {
-    "token": "234959khskfhir3945h",
-    "username": "yugm",
+    "token": USERNAME,
+    "username": TOKEN,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
-print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+requests.post(url=graph_endpoint, )
