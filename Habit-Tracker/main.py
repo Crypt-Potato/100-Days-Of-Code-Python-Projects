bccd441e1594f21706a05122ec7a91ea4791eb9f
arham shah <yugsha28@gmail.com>
2023-02-11 17:31:56 -0500

    more work done

--- Habit-Tracker/main.py
+++ Habit-Tracker/main.py
@@ -2,7 +2,16 @@ import requests
 import os
 from datetime import datetime
 
-now = str(datetime.now())
+now = datetime.now()
+
+date_input = input("Please put the day that you did it. Format: <year> <month(number)> <day(number)>"
+                          "Input \"today\" to use today's date.")
+
+if date_input == "today":
+    date_to_put_pixel = now.strftime("%Y%m%d")
+else:
+    date_inputted = date_input.split()
+    date_to_put_pixel = datetime(year=int(date_inputted[0]), month=int(date_inputted[1]), day=int(date_inputted[2])).strftime("%Y%m%d")
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -39,14 +48,10 @@ headers = {
 
 pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
 
-pixel_creation_config = {
-    "date": "".join(now.split()[0].split("-")),
+pixel_data = {
+    "date": date_to_put_pixel,
     "quantity": "5"
 }
 
-pixel_creation_headers = {
-    "X-USER-TOKEN": TOKEN
-}
-
-response = requests.post(url=pixel_creation_endpoint, json=pixel_creation_config, headers=pixel_creation_headers)
+response = requests.post(url=pixel_creation_endpoint, json=pixel_data, headers=headers)
 print(response.text)
