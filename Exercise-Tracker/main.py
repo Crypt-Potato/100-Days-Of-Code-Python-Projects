8d658f28d8566f55bcb708dafa544dc87a0a1bcb
arham shah <yugsha28@gmail.com>
2023-02-27 21:26:23 -0500

    finishing my exercise tracker project

--- Exercise-Tracker/main.py
+++ Exercise-Tracker/main.py
@@ -1,12 +1,18 @@
 import os
 import requests
+from datetime import datetime
+
+now = datetime.now()
+
+date = now.strftime("%m/%d/%Y")
+time = now.strftime('%I:%M %p')
 
 APP_ID = os.environ["APP_ID"]
 API_KEY = os.environ["API_KEY"]
 
-query = input("Input you exercises: ")
+query = input("Input your exercises (in metric form): ")
 
-endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+exercise_data_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 headers = {
     "x-app-id": APP_ID,
@@ -14,12 +20,34 @@ headers = {
     "Content-Type": "application/json"
 }
 
-json = {
+exercise_data_json = {
     "query": query,
     "gender": os.environ["GENDER"],
-    "weight_lb": os.environ["WEIGHT"],
-    "height_in": os.environ["HEIGHT"],
+    "weight_kg": os.environ["WEIGHT"],
+    "height_cm": os.environ["HEIGHT"],
     "age": os.environ["AGE"]
 }
+response = requests.post(url=exercise_data_endpoint, headers=headers, json=exercise_data_json)
+exercise_data = response.json()
 
+sheet_endpoint = os.environ["SHEETY_ENDPOINT"]
 
+sheet_headers = {
+    "Authorization": os.environ["SHEETY_AUTH"]
+}
+
+for exercise in exercise_data["exercises"]:
+
+    sheet_inputs = {
+        "workout": {
+            "date": str(date),
+            "time": str(time),
+            "exercise": str(exercise["name"]).title(),
+            "duration": str(exercise['duration_min']),
+            "calories": str(exercise['nf_calories'])
+        }
+    }
+
+    sheet_response = requests.post(url=sheet_endpoint, json=sheet_inputs,headers=sheet_headers)
+
+    print(sheet_response.text)
