e93133d9b52ddecadb11ea31521b2d90bfbcf1c5
arham shah <yugsha28@gmail.com>
2023-03-01 19:48:12 -0500

    Revert "Revert "started and almost done with part 1 of cheap flight tracker""
    This reverts commit c543c820b8b10243832cfdefa1f2dc1b6556480d.

---
+++ Cheap-Flight-Tracker/main.py
@@ -0,0 +1,34 @@
+from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
+
+data_manager = DataManager()
+flight_search = FlightSearch()
+sheet_data = data_manager.get_dest_data()
+is_iata_code = None
+
+origin_city_iata = "LON"
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        is_iata_code = False
+        break
+    else:
+        is_iata_code = True
+
+if not is_iata_code:
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_dest_code(row["city"])
+        data_manager.data = sheet_data
+    data_manager.update_rows()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_months_from_now = datetime.now() + timedelta(days=(6 * 30))
+
+for dest in sheet_data:
+    flight = flight_search.check_flights(
+        origin_code=origin_city_iata,
+        dest_code=dest["iataCode"],
+        from_time=tomorrow,
+        to_time=six_months_from_now
+    )
